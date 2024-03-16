d1fc4777ef9c5f665420c4a69d4954c740bc5f51
Colin Barnabas <1637663+colin-barnabas@users.noreply.github.com>
2024-03-15 22:02:35 -0700

    Allow passing interface option

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,12 @@
 
 setopt local_options
 
-local -a trace
-zparseopts -D -E -- x=trace
+local -A opts
+local -a trace iface
+zparseopts -A opts -D -E -- i:-=iface x=trace
 if [[ "x$trace" != 'x' ]]; then setopt xtrace; fi
 
 test $ARGC -gt 0 \
-  && eval -- test\ -x\ $(whence -p $1 || echo /dev/null) \
+  && eval -- test\ -x\ $(whence -p -- $1 || echo /dev/null) \
   && eval -- sh\ -c\ ${(qqf)@} \
-  || eval -- exec\ termshark\ -i\ any\ ${(qqf)@}
+  || eval -- exec\ termshark\ -i\ ${(v)opts[-i]:-any} ${(qqf)@}
