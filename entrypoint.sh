40a991ec98e9b4f7290090bd8ab914a41af03ed3
Colin Barnabas <1637663+colin-barnabas@users.noreply.github.com>
2024-03-15 21:33:43 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
-#! /bin/zsh
+#! /usr/bin/env zsh
 
-test -n "$argv" \
-  && eval -- test\ -x\ $(>&2 which $1 || echo /dev/null) \
-  && eval -- sh -c "'$@'" \
-  || exec termshark -i any "$@"
+
+setopt local_options
+
+local -a trace
+zparseopts -D -E -- x=trace
+if [[ "x$trace" != 'x' ]]; then setopt xtrace; fi
+
+test $ARGC -gt 0 \
+  && eval -- test\ -x\ $(whence -p $1 || echo /dev/null) \
+  && eval -- sh\ -c\ ${(qqf)@} \
+  || eval -- exec\ termshark\ -i\ any\ ${(qqf)@}
